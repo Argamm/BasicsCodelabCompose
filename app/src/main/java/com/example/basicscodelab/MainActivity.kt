4054f6655c75aa5fc4be27fe7e5a34ccefaf4ee4
Aeg <mehrabyanargam111@gmail.com>
2023-11-08 17:21:53 +0400

    final version LazyColumn, Row, Column, Text, IconButton, Icon, Card, Button, themes

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +43,200 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun CardContent(
+    name: String,
+    position: Int,
+) {
+    var expanded by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = "$name $position", style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+
+            if (expanded) {
+
+                Text(
+                    modifier = Modifier.padding(bottom = 24.dp),
+                    text = "This is connected for $name." +
+                            "And thair position is $position" +
+                            "Afer this imformation we will be more powerful and clever. ",
+                    style = MaterialTheme.typography.headlineSmall.copy(
+                        fontWeight = FontWeight.Medium
+                    )
+                )
+            }
+
+        }
+        val context = LocalContext.current
+        IconButton(
+            onClick = {
+                expanded = !expanded
+            },
+        ) {
+            Icon(
+                modifier = Modifier,
+                imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+private fun Greeting(
+    name: String,
+    position: Int,
+) {
+
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name, position)
+    }
+
+
+}
+
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf(
+        "word",
+        "composerg eredrsrgres gdsfg sdfg dsfgdfsg sdfg dsfgdfg sdgf sdfg dafgsad gf",
+        "Gago",
+        "word",
+        "compose",
+        "Gago",
+        "word",
+        "compose",
+        "Gago",
+        "word",
+        "compose",
+        "Gago",
+        "word",
+        "compose",
+        "Gago"
+    )
+) {
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboardingScreen(Modifier) {
+            shouldShowOnboarding = false
+        }
+    } else {
+        LazyColumn(
+            modifier = modifier,
+            content = {
+                items(names.size) { position ->
+                    Greeting(name = names[position], position = position)
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit
+) {
+    var shouldShowOnboarding by remember {
+        mutableStateOf(true)
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Welcome to My Compose app")
+        Button(modifier = Modifier.padding(vertical = 24.dp),
+            onClick = {
+                onContinueClicked.invoke()
+                shouldShowOnboarding = false
+            }) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        Greeting("", 1)
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    BasicsCodelabTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingScreenPrewiew() {
+    OnboardingScreen() {}
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingPreviewLi() {
+    BasicsCodelabTheme {
+        MyApp(
+            modifier = Modifier,
+            names = listOf(
+                "word", "compose", "Gago",
+                "word", "compose", "Gago",
+                "word", "compose", "Gago",
+                "word", "compose", "Gago",
+                "word", "compose", "Gago"
+            )
+        )
     }
 }
